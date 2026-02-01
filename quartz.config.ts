22c8d333d34204a8ca89206f153bf548c5f1b5d7
Ernesto-B <ernestostar100@gmail.com>
2026-01-31 17:38:22 -0700

    fix: fix base url again again

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://ernesto-b.github.io/miskar-worldbuilding",
+    baseUrl: "ernesto-b.github.io/miskar-worldbuilding",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
